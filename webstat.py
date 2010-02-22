c123e207b3a57bfd8982cfda65c070269be54f38
Vycas <vycas0@gmail.com>
2010-02-22 17:56:43 +0200

    Working program

--- webstat.py
+++ webstat.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python
 
+import sys
+import hashlib
+from urllib import urlopen
+from datetime import datetime
 
+url = sys.argv[1]
+start = datetime.now()
+try:
+    site = urlopen(url)
+except IOError:
+    print 'Unable to open given url.'
+    quit()
+end = datetime.now()
+time = end - start
+body = site.read()
+size = len(body)
+speed = size * 1000.0 / time.microseconds
+lines = body.count('\n') + 1
+hash = hashlib.sha1(body)
+
+print 'Speed: %.2f KB/s' % speed
+print 'SHA-1:', hash.hexdigest()
+print 'Size:', size
+print 'Lines:', lines
